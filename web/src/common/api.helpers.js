d8a57622050368d82bee0f9337cc24d2d999924e
tvaisanen <vaisanen.toni@gmail.com>
2018-10-29 15:36:41 +0200

    removed accidental line

--- web/src/common/api.helpers.js
+++ web/src/common/api.helpers.js
@@ -5,7 +5,6 @@ export function handleNetworkError(error){
 
     return function (dispatch, getState) {
         console.group("handleNetworkError");
-        dispatch();
         console.info(error);
         console.info(dispatch);
         console.info(getState);
