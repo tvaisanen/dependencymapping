9598ece444fcfb89838e50f4477e5c520f09d19d
tvaisanen <vaisanen.toni@gmail.com>
2018-11-20 09:41:00 +0200

    two more tests for mapping api

--- test-moin-api/tests/test_mapping.py
+++ test-moin-api/tests/test_mapping.py
@@ -30,3 +30,27 @@ def test_get_mappings_without_id_returns_mapping_name_list_with_status_200():
 
     for item in response_mappings:
         assert item in mappings
+
+
+def test_get_not_existing_mapping_returns_404():
+
+    query_url = paths.get_resource_path(MAPPING, "MappingDoNotExist")
+    r = requests.get(query_url, verify=False, auth=credentials)
+
+    assert r.status_code == 404
+
+
+def test_get_mapping_by_id_returns_correctly_with_200():
+
+    expect = mappings[0]
+
+    query_url = paths.get_resource_path(MAPPING, "TestMappingOne")
+    r = requests.get(query_url, verify=False, auth=credentials)
+
+    data = load_json(r)
+
+    assert data[NAME] == expect[NAME]
+    assert data[DESCRIPTION] == expect[DESCRIPTION]
+    assert data[ASSETS] == expect[ASSETS]
+    assert data[TAGS] == expect[TAGS]
+
