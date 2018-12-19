5550efcf5178e7d468e30d964422545e77e9e591
tvaisanen <vaisanen.toni@gmail.com>
2018-12-19 14:01:24 +0200

    cleaning the repo

--- dependencymapping/application/tests.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-
-# Create your tests here.
-class AssetModelTest(TestCase):
-
-    def test_string_representation(self):
-        self.fail("TODO Test incomplete")
