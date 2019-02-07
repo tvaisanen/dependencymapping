e4002204acffa97f58f7739bbe4cef74c6e624f9
tvaisanen <vaisanen.toni@gmail.com>
2019-02-07 16:21:48 +0200

    push for office change

--- services/express-server/src/routers/asset.router.js
+++ services/express-server/src/routers/asset.router.js
@@ -2,6 +2,7 @@ const Router = require('express').Router;
 const assetRouter = Router();
 const AssetController = require('../controllers/asset.controller');
 
+// https://app.swaggerhub.com/apis-docs/tvaisanen_dev/dependency-maps/1.0.0-oas3
 assetRouter.get('/',        (req,res) => AssetController.get(req,res));
 assetRouter.get('/:id',     (req,res) => AssetController.getById(req,res));
 assetRouter.post('/',       (req,res) => AssetController.post(req,res));
--- services/express-server/src/routers/connection.router.js
+++ services/express-server/src/routers/connection.router.js
@@ -2,31 +2,11 @@ const express = require('express');
 const Connection = require('../models').Connection;
 const connectionRouter = express.Router();
 const hal = require('../utils/hal.utils');
+const Router = require('express').Router;
+const ConnectionController = require('../controllers/connection.controller');
 
-connectionRouter.get('(/:id)?', (req, res) => {
-    if (req.query.source || req.query.target) {
-        console.log("connection");
-        console.log(req.query);
-        Connection.findOne(req.query)
-            .then(tag => {
-                if (tag == null) {
-                    console.log("connection does not exist.");
-                    res.status(404).json("Resource does not exist.")
-                } else {
-                    console.log(tag);
-                    res.status(200).json(tag);
-                }
-            }).catch(err => res.status(400).json(err));
 
-    } else {
-        Connection.find()
-            .then(connections => {
-                const HALConnections = connections.map(c => hal.serializeConnection(req.headers.host, c));
-                res.status(200).json(HALConnections)
-            })
-            .catch(err => res.status(400).json(err));
-    }
-});
+connectionRouter.get('(/:id)?', (req,res) => ConnectionController.get(req,res));
 
 
 connectionRouter.delete('(/:id)?', (req, res) => {
