07b8e03fc4f09cd281dd587ea90c42e9c90f0d30
tvaisanen <vaisanen.toni@gmail.com>
2019-03-15 13:46:41 +0200

    asset delete by id removes connections

--- services/express-server/src/controllers/asset-controller/asset.deleteById.js
+++ services/express-server/src/controllers/asset-controller/asset.deleteById.js
@@ -4,8 +4,16 @@ function assetDelete(req, res){
     console.log(`assetDeleteById:: ${req.params.id}`)
     console.log(req.params)
     const query = {_id: req.params.id};
-    Asset.deleteOne(query)
-        .then(ok => {
+    Asset.findOneAndDelete(query)
+        .then(asset => {
+
+            // todo: move to the model
+            asset.connected_to.forEach(target => {
+                Connection.deleteMany({$or: [{source: asset.name}, {target: asset.name}]})
+                    .then((ok) => console.log(ok))
+                    .catch(err => console.log(err))
+            });
+
             res.status(204).json();
         })
         .catch(err => res.status(400).json(err))
